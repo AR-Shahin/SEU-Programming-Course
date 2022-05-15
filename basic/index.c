858224881855cee514de1ccca304bd7ddaa96614
ar-shahin <mdshahinmije96@gmail.com>
2022-05-15 23:25:12 +0600

    c programing

---
+++ basic/index.c
@@ -0,0 +1,17 @@
+// Write a program to print hello world
+
+#include <stdio.h>
+
+#define PI 3.1416
+
+// Global
+
+int test();
+int x;
+int y;
+
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
---
+++ basic/index.cpp
@@ -0,0 +1,10 @@
+// Docutentation
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+    return 0;
+}
