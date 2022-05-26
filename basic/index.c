9ca64b9cd292e4479ed1cf93e4f06a61d19ce203
ar-shahin <mdshahinmije96@gmail.com>
2022-05-26 22:29:45 +0600

    area of triangle

--- basic/index.c
+++ basic/index.c
@@ -14,47 +14,13 @@ Multiline comment
 int main()
 {
 
-    const double PI = 3.1416;
-    // PI = 10.6;
-    int num1, num3, num4;           // declare
-    float num2 = 10.5, num5 = 0.56; // declare and assign
-    int _var1;
-    double numOne = 10.36; // Camel Case
-    double num_one;        // Snake Case
+    float b, h, area;
+    printf("Enter base and height : \n");
+    scanf("%f%f", &b, &h);
 
-    int z = 10;
-    char ch = '#';
+    area = 0.5 * h * b;
 
-    // // bool isMarried = 0;
+    printf("Area = %.2f ", area);
 
-    // num1 = 10;
-    // num1 = 20;
-
-    // printf("Int = %d\n", num1);
-    // printf("Float = %f\n", num2);
-    // printf("Double = %lf\n", numOne);
-    // printf("Char = %c\n", ch);
-
-    // printf("Enter a number : \n");
-    // scanf("%d %d %f", &num3, &num4, &num2);
-    // printf("Your number is : %d\n", num3);
-
-    // printf("%d\n", &num1);
-    // printf("%d",sizeof(ch));
-
-    int a;
-    int b;
-
-    // printf("Sum is : %d\n", a + b);
-    // printf("Subs is : %d\n", a - b);
-    // printf("Multiply is : %d\n", a * b);
-    // printf("Div is : %d\n", a / b);
-    // printf("Mod is : %d\n", a % b);
-
-    // printf("%d", 10 / 3);
-
-    scanf("%d%d", &a, &b);
-    float avg = (a+b)/2.0;
-    printf("Avg %f", avg);
     return 0;
 }
--- basic/index.js
+++ basic/index.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+let time = new Date();
+let add_time = 0;
+let start_time = 0;
+let prev_time = '10:30';
+
+for(let i = 1;i<=15;i++){
+    add_time =+ 30;
+    if(i == 1){
+        prev_time = prev_time;
+    }else{
+        prev_time = start_time;
+    }
+    start_time = Date("h:i");
+}
+console.log(start_time);
