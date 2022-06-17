9b9cac59c2ee6bc71727c1b7b819aace595f4409
AR-Shahin <mdshahinmije96@gmail.com>
2022-06-17 22:15:11 +0600

    basic salary problem

--- basic/index.c
+++ basic/index.c
@@ -1,17 +1,19 @@
-// Write a program that convert farenheight to celcious
+
 
 #include <stdio.h>
 
-#define MU 3.25
 int main()
 {
 
-    int a = 10, b = 5, temp;
-    printf("before swapping %d -> %d\n", a, b);
-    temp = a; // a is empty now
-    a = b;    // b is empty now and a has b
-    b = temp; // temp is empty now
+    double salary,mw,hw,ex,total;
+    printf("Enter Basic Salary : ");
+    scanf("%lf",&salary);
 
-    printf("%d -> %d", a, b);
+    hw = salary * 0.3;
+    mw = salary * 0.05;
+    ex = 1000;
+
+    total = salary + hw + mw + ex;
+    printf("Gross Salary : %lf",total);
     return 0;
 }
--- basic/index.cpp
+++ basic/index.cpp
@@ -1,10 +1,20 @@
-// Docutentation
+
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!";
+    double salary,mw,hw,ex,total; 
+    cout<<"Enter Salary : ";
+    cin>>salary;
+
+    hw = salary * 0.3;
+    mw = salary * 0.05;
+    ex = 1000;
+
+    total = salary + hw + mw + ex;
+
+    cout<<"Gross Salary " <<total;
     return 0;
 }
