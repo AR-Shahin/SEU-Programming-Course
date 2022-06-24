117c0f6a2501fdb90c7b4ce9d79f84c0cbc19834
AR-Shahin <mdshahinmije96@gmail.com>
2022-06-24 22:20:16 +0600

    grade system

--- basic/index.c
+++ basic/index.c
@@ -3,16 +3,38 @@
 int main()
 {
 
-    int sell,cost;
-    scanf("%d%d",&sell,&cost);
-
-    if(sell > cost){
-        printf("Profit! %d",sell - cost);
-    }else{
-        printf("Loss! %d",cost - sell);
-
-    }
+    int mark;
+    scanf("%d",&mark);
     
+    if(mark >= 80 && mark <= 100){
+        printf("A+");
+    }
+    else if(mark >= 75 && mark <= 79){
+        printf("A");
+    }
+    else if(mark >= 70 && mark <= 74){
+        printf("A-");
+    }
+    else if(mark >= 70 && mark <= 74){
+        printf("A-");
+    }
 
+    else if(mark >= 65 && mark <= 69){
+        printf("B+");
+    }
+
+    else if(mark >= 60 && mark <= 64){
+        printf("B");
+    }
+     else if(mark >= 55 && mark <= 59){
+        printf("B-");
+    }
+
+    // I skipped a couple of conditions 
+    
+    else if(mark >= 0 && mark < 40){
+        printf("F");
+    }
     return 0;
 }
+// . [100 – 80 = ‘A+’, 75 – 79 = ‘A’, 70 – 74 = A-, 65 – 69 = B+, 60 – 64 = B, 55 -59 = B-, 50 – 54 = C+, 45 – 49 = C, 40 – 44 = D, 0 – 39 = F]
