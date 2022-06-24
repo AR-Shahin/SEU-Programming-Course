89ce4b75499288b9923dbf39c743b48b915f4a2c
AR-Shahin <mdshahinmije96@gmail.com>
2022-06-24 22:33:48 +0600

    basic switch case

--- basic/index.c
+++ basic/index.c
@@ -3,14 +3,17 @@
 int main()
 {
 
-    char ch;
-    scanf("%c",&ch);
+    int x = 5;
+    switch(x){
+        case 5:
+            printf("Five");
+            break;
+        case 10:
+            printf("Ten");
+            break;
+        default:
+            printf("Default");
 
-    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-        printf("Vowel");
-    }else{
-        printf("Consonant!");
     }
     return 0;
 }
-// . [100 – 80 = ‘A+’, 75 – 79 = ‘A’, 70 – 74 = A-, 65 – 69 = B+, 60 – 64 = B, 55 -59 = B-, 50 – 54 = C+, 45 – 49 = C, 40 – 44 = D, 0 – 39 = F]
